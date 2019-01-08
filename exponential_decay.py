c40116af9f7b97f6092bf31d04da946819daf210
Ognjen Kocić <zbetmen1@gmail.com>
2019-01-08 17:11:02 +0100

    Adding minimal lr support.

--- exponential_decay.py
+++ exponential_decay.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 
 class ExponentialDecay(tf.keras.callbacks.Callback):
-    def __init__(self, decay_rate, decay_steps, staircase=False, verbose_steps=None):
+    def __init__(self, decay_rate, decay_steps, minimal_lr=1e-6, staircase=False, verbose_steps=None):
         super(ExponentialDecay, self).__init__()
         self.decay_rate = decay_rate
         self.decay_steps = decay_steps
+        self.minimal_lr = minimal_lr
         self.staircase = staircase
 
         self.step = 0
@@ -23,7 +24,8 @@ class ExponentialDecay(tf.keras.callbacks.Callback):
 
         # Print base learning rate if verbose
         if self.verbose_steps is not None and epoch == 0:
-            print('Exponential decay: base learning rate {}.'.format(self.base_lr))
+            print('Exponential decay: base learning rate {} minimal learning rate {}.'.format(self.base_lr,
+                                                                                              self.minimal_lr))
 
     def on_batch_begin(self, batch, logs=None):
         # Check for learning rate
@@ -32,7 +34,7 @@ class ExponentialDecay(tf.keras.callbacks.Callback):
 
         # Apply decay formula and set model learning rate
         power = self.step / self.decay_steps if not self.staircase else self.step // self.decay_steps
-        lr = self.base_lr * (self.decay_rate ** power)
+        lr = max(self.base_lr * (self.decay_rate ** power), self.minimal_lr)
         tf.keras.backend.set_value(self.model.optimizer.lr, lr)
 
         # Update step and print if verbose
